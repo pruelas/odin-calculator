1c91fc3e105b0c144dd3037ccdffd774ccaa9ce2
Pedro Ruelas <pruelas5238@yahoo.com>
2022-10-20 16:17:49 -0700

    Added comments to javascript file

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # odin-calculator
+========================
+An onlince calculator created by following [The Odin Project](#https://www.theodinproject.com/lessons/foundations-calculator)
+
+
+## Features
+========================
+* Addition, subtraction, multiplication and division are supported
+* Calculator evaluates one single pair of numbers at a time
+ * Does not follow PEMDAS
+* Has a character limit of 8 for the display screen
+* "clear" key wipes out all existing data
+* "del" key allows the user to undo if a wrong number was clicked
--- odin-calculator.js
+++ odin-calculator.js
@@ -1,3 +1,4 @@
+//Evaluates current expression 
 function operate(operator, number1, number2){
     var solution ="";
     if(number1 == ""){
@@ -11,6 +12,7 @@ function operate(operator, number1, number2){
     }else if(operator == ""){
         solution = number2;
     }else{
+        //Returns error if user is dividing by zero
         if(isNaN(Number(number1)/Number(number2))){
             solution = "Nope";
        }else{
@@ -18,6 +20,7 @@ function operate(operator, number1, number2){
        }
     }
 
+    //Ensures solution is at 8 characters or below
     if(solution.toString().length > 8 && !isNaN(solution)){
         if(Number.isInteger(solution)){
             return "NaN";
@@ -40,6 +43,7 @@ var expression = "";
 var prevOperand = "0";
 var currOperand = "";
 var prevOperator = "";
+//Event listener executes when a key on calculator is clicked
 window.addEventListener('click', function(e, expresssion){
     var displayContent = document.getElementById("display");
 
@@ -139,17 +143,20 @@ window.addEventListener('click', function(e, expresssion){
         }
     }
 
+    //Resets operand variables if division by zero occurred or calculation exceeded the 8 character limit
     if(displayContent.textContent == "Nope" || displayContent.textContent == "NaN"){
         prevOperand = "";
         currOperand = "";
     }
 });
 
+//Event listerner allows numpad on eyboard to be used to input operands and operators
 window.addEventListener('keydown', function(e, expresssion){
+    console.log(e.code);
     var displayContent = document.getElementById("display");
 
     if(currOperand.length < 8 && !isNaN(e.code.charAt(e.code.length-1))){
-        if(e.code == "Numpad0"){
+        if(e.code == "Numpad0" || e.code == "Digit0"){
             currOperand += "0";
             displayContent.textContent = currOperand;
 
@@ -161,31 +168,31 @@ window.addEventListener('keydown', function(e, expresssion){
             currOperand += "2";
             displayContent.textContent = currOperand;
 
-        }else if(e.code == "Numpad3"){
+        }else if(e.code == "Numpad3" || e.code == "Digit3"){
             currOperand += "3";
             displayContent.textContent = currOperand;
 
-        }else if(e.code == "Numpad4"){
+        }else if(e.code == "Numpad4" || e.code == "Digit4"){
             currOperand += "4";
             displayContent.textContent = currOperand;
 
-        }else if(e.code == "Numpad5"){
+        }else if(e.code == "Numpad5" || e.code == "Digit5"){
             currOperand += "5";
             displayContent.textContent = currOperand;
 
-        }else if(e.code == "Numpad6"){
+        }else if(e.code == "Numpad6" || e.code == "Digit6"){
             currOperand += "6";
             displayContent.textContent = currOperand;
 
-        }else if(e.code == "Numpad7"){
+        }else if(e.code == "Numpad7" || e.code == "Digit7"){
             currOperand += "7";
             displayContent.textContent = currOperand;
 
-        }else if(e.code == "Numpad8"){
+        }else if(e.code == "Numpad8" || e.code == "Digit8"){
             currOperand += "8";
             displayContent.textContent = currOperand;
 
-        }else if(e.code == "Numpad9"){
+        }else if(e.code == "Numpad9" || e.code == "Digit9"){
             currOperand += "9";
             displayContent.textContent = currOperand;
         }
@@ -240,7 +247,7 @@ window.addEventListener('keydown', function(e, expresssion){
             currOperand = "";
         }
     }
-
+    //Resets operand variables if division by zero occurred or calculation exceeded the 8 character limit
     if(displayContent.textContent == "Nope" || displayContent.textContent == "NaN"){
         prevOperand = "";
         currOperand = "";
